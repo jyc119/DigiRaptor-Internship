ddc750e50acc4b338ba0ae01695f7117e0c8cbfe
jyc119 <60002815+jyc119@users.noreply.github.com>
2022-11-29 14:04:58 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<div class="card-action right-align">
+  <a href="staff/staffPage.php" class="brand-text">Staff Page</a>
+</div>
+
+<div class="card-action right-align">
+  <a href="project/project.php" class="brand-text">Project Page</a>
+</div>
+
+<div class="card-action right-align">
+  <a href="staff_project/staffproj.php" class="brand-text">Staff Project Page</a>
+</div>
+
+</html> 
